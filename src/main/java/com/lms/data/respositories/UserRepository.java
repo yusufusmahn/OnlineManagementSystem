10aecbfa212718723878a9e24d3c5e0ee43a1e5e
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:14:06 +0100

    new task added

--- src/main/java/com/lms/data/respositories/UserRepository.java
+++ src/main/java/com/lms/data/respositories/UserRepository.java
@@ -1,8 +1,10 @@
 package com.lms.data.respositories;
 
+import com.lms.data.models.Role;
 import com.lms.data.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +13,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCase(String email);
 
+    List<User> findByRole(Role role);
 
 }
