41943fbee78556b117d0683e74339f30bed58947
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:15:08 +0100

    update

---
+++ src/main/java/com/lms/dto/requests/PasswordResetRequest.java
@@ -0,0 +1,9 @@
+// src/main/java/com/lms/dto/requests/PasswordResetRequest.java
+package com.lms.dto.requests;
+
+import lombok.Data;
+
+@Data
+public class PasswordResetRequest {
+    private String email;
+}
