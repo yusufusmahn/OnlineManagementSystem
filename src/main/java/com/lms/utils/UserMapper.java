e3f2170b56f64949f1c64c8eb1ba30f8afe25570
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:14:12 +0100

    new task added

--- src/main/java/com/lms/utils/UserMapper.java
+++ src/main/java/com/lms/utils/UserMapper.java
@@ -3,7 +3,7 @@ package com.lms.utils;
 
 import com.lms.data.models.User;
 import com.lms.dto.requests.UserRequestDTO;
-import com.lms.dto.responses.UserResponseDTO;
+import com.lms.dto.responses.UserResponse;
 
 public class UserMapper {
 
@@ -17,13 +17,15 @@ public class UserMapper {
         return user;
     }
 
-    public static UserResponseDTO toDTO(User user) {
-        UserResponseDTO dto = new UserResponseDTO();
+    public static UserResponse toDTO(User user) {
+        UserResponse dto = new UserResponse();
         dto.setId(user.getId());
         dto.setName(user.getName());
         dto.setEmail(user.getEmail());
         dto.setRole(user.getRole()); //  correct direction
         return dto;
     }
+
+
 
 }
