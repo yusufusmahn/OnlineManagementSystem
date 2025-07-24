cc2f5a02d28d74ff95ca7c028daf2afc260dcc89
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:21:08 +0100

    update

---
+++ src/main/java/com/lms/data/respositories/AssignmentSubmissionRepository.java
@@ -0,0 +1,14 @@
+package com.lms.data.respositories;
+
+import com.lms.data.models.AssignmentSubmission;
+import com.lms.data.models.Course;
+import com.lms.data.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AssignmentSubmissionRepository extends JpaRepository<AssignmentSubmission, Long> {
+    List<AssignmentSubmission> findByCourse(Course course);
+    List<AssignmentSubmission> findByStudent(User student);
+
+}
