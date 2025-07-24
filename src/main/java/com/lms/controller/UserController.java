f18b83b7fbca7b59a7e2dd06fabd06139ef851e0
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:14:19 +0100

    new task added

--- src/main/java/com/lms/controller/UserController.java
+++ src/main/java/com/lms/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.lms.controller;
 
+import com.lms.dto.requests.CreateInstructorRequestDTO;
 import com.lms.dto.requests.UserRequestDTO;
-import com.lms.dto.responses.UserResponseDTO;
+import com.lms.dto.responses.UserResponse;
 import com.lms.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,22 +22,22 @@ public class UserController {
 
     // Get user by ID
     @GetMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id) {
-        UserResponseDTO response = userService.getUserById(id);
+    public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
+        UserResponse response = userService.getUserById(id);
         return ResponseEntity.ok(response);
     }
 
     // Get all users
     @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
-        List<UserResponseDTO> users = userService.getAllUsers();
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
     // Update user
     @PutMapping("/{id}")
-    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID id, @RequestBody UserRequestDTO dto) {
-        UserResponseDTO response = userService.updateUser(id, dto);
+    public ResponseEntity<UserResponse> updateUser(@PathVariable UUID id, @RequestBody UserRequestDTO dto) {
+        UserResponse response = userService.updateUser(id, dto);
         return ResponseEntity.ok(response);
     }
 
@@ -45,4 +47,23 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping("/instructors")
+    public ResponseEntity<UserResponse> createInstructor(@RequestBody CreateInstructorRequestDTO dto) {
+        UserResponse instructor = userService.createInstructor(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(instructor);
+    }
+
+    @DeleteMapping("/instructors/{instructorId}")
+    public ResponseEntity<?> deleteInstructor(@PathVariable UUID instructorId) {
+        userService.deleteInstructor(instructorId);
+        return ResponseEntity.ok("Instructor deleted successfully.");
+    }
+
+
+    @GetMapping("/instructors")
+    public ResponseEntity<List<UserResponse>> getAllInstructors() {
+        return ResponseEntity.ok(userService.getAllInstructors());
+    }
+
 }
