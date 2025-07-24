e65a6e2a089e25ca195963493e0b8d72aa804d65
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:13:54 +0100

    new task added

--- src/main/java/com/lms/services/UserService.java
+++ src/main/java/com/lms/services/UserService.java
@@ -1,16 +1,21 @@
 package com.lms.services;
 
 
+import com.lms.dto.requests.CreateInstructorRequestDTO;
 import com.lms.dto.requests.UserRequestDTO;
-import com.lms.dto.responses.UserResponseDTO;
+import com.lms.dto.responses.UserResponse;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    UserResponseDTO createUser(UserRequestDTO dto);
-    UserResponseDTO getUserById(UUID id);
-    List<UserResponseDTO> getAllUsers();
-    UserResponseDTO updateUser(UUID id, UserRequestDTO dto);
+    UserResponse createUser(UserRequestDTO dto);
+    UserResponse getUserById(UUID id);
+    List<UserResponse> getAllUsers();
+    UserResponse updateUser(UUID id, UserRequestDTO dto);
     void deleteUser(UUID id);
+    UserResponse createInstructor(CreateInstructorRequestDTO dto);
+    void deleteInstructor(UUID instructorId);
+    List<UserResponse> getAllInstructors();
+
 }
