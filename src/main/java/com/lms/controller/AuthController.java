6ca91cf1a408984fbc700987a8b92ff3472c89b6
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:20:33 +0100

    update

--- src/main/java/com/lms/controller/AuthController.java
+++ src/main/java/com/lms/controller/AuthController.java
@@ -2,11 +2,13 @@ package com.lms.controller;
 
 
 import com.lms.dto.requests.LoginRequest;
+import com.lms.dto.requests.PasswordResetRequest;
 import com.lms.dto.requests.UserRequestDTO;
 import com.lms.dto.responses.LoginResponse;
-import com.lms.dto.responses.UserResponseDTO;
+import com.lms.dto.responses.UserResponse;
 import com.lms.services.AuthenticationService;
 import com.lms.services.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,12 +24,27 @@ public class AuthController {
 
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponseDTO> signup(@RequestBody UserRequestDTO dto) {
+    public ResponseEntity<UserResponse> signup(@RequestBody UserRequestDTO dto) {
         return ResponseEntity.ok(userService.createUser(dto));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authenticationService.login(request));
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        return authenticationService.login(request, response);
     }
+
+    @PostMapping("/auth/request-password-reset")
+    public ResponseEntity<?> requestPasswordReset(@RequestParam String email) {
+        authenticationService.requestPasswordReset(email);
+        return ResponseEntity.ok("Password reset link sent to email");
+    }
+
+    @PostMapping("/auth/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestParam String token,
+                                           @RequestParam String newPassword) {
+        authenticationService.resetPassword(token, newPassword);
+        return ResponseEntity.ok("Password updated successfully");
+    }
+
+
 }
