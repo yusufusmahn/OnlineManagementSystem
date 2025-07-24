b0b6a55ae752ef497082569a93883c3e05c36f3b
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:13:00 +0100

    new task added

---
+++ src/main/java/com/lms/data/models/AssignmentSubmission.java
@@ -0,0 +1,44 @@
+package com.lms.data.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AssignmentSubmission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "assignment_id")
+    private Assignment assignment;
+
+
+    @Lob
+    private String content;
+
+    private String grade; // Add this field
+
+    private LocalDateTime submittedAt;
+
+    @PrePersist
+    public void setSubmittedAt() {
+        this.submittedAt = LocalDateTime.now();
+    }
+}
