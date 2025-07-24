22e879ed1fdf359ef90cca261c36be4ab5b1eb4d
yusufusman <yusuf.usman1003@gmail.com>
2025-07-24 15:11:54 +0100

    new task added

---
+++ src/main/java/com/lms/data/models/Assignment.java
@@ -0,0 +1,43 @@
+package com.lms.data.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    private String fileUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime dueDate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
