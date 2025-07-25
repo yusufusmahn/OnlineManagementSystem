172c70c8b4f6357ea4e880129e3d9703ace7cf23
yusufusman <yusuf.usman1003@gmail.com>
2025-07-25 10:13:46 +0100

    update

--- src/main/java/com/lms/services/AssignmentSubmissionService.java
+++ src/main/java/com/lms/services/AssignmentSubmissionService.java
@@ -1,6 +1,7 @@
 package com.lms.services;
 
 import com.lms.dto.requests.AssignmentSubmissionRequest;
+import com.lms.dto.requests.GradeSubmissionRequest;
 import com.lms.dto.responses.AssignmentSubmissionResponse;
 
 import java.util.List;
@@ -9,8 +10,9 @@ public interface AssignmentSubmissionService {
     AssignmentSubmissionResponse submitAssignment(AssignmentSubmissionRequest request);
     List<AssignmentSubmissionResponse> getMySubmissions();
     List<AssignmentSubmissionResponse> getSubmissionsByCourse(Long courseId);
-    AssignmentSubmissionResponse gradeSubmission(Long submissionId, String grade);
+    AssignmentSubmissionResponse gradeSubmission(Long submissionId, GradeSubmissionRequest request);
     AssignmentSubmissionResponse updateSubmission(Long submissionId, AssignmentSubmissionRequest request);
+    List<AssignmentSubmissionResponse> getGradedSubmissionsForCurrentStudent();
 
 
 }
