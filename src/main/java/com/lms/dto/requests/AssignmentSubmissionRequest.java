d70366fc454c7a8283bc0bbb9bb63164ad9c158e
yusufusman <yusuf.usman1003@gmail.com>
2025-07-25 10:13:33 +0100

    update

--- src/main/java/com/lms/dto/requests/AssignmentSubmissionRequest.java
+++ src/main/java/com/lms/dto/requests/AssignmentSubmissionRequest.java
@@ -1,9 +1,12 @@
 package com.lms.dto.requests;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class AssignmentSubmissionRequest {
     private Long courseId;
     private String content;
+    private MultipartFile file; // for the uploaded file
+
 }
